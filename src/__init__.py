4b493cd53d9e81e9a607f09d205db9e335746b96
benluks <70067243+benluks@users.noreply.github.com>
2021-10-19 19:00:12 +0200

    dynamically create data dictionary. still problems with module imports

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from . import *
+from .utils.time import save_data_to_memory
+import src.utils
--- src/speak/__init__.py
+++ src/speak/__init__.py
@@ -2,3 +2,5 @@
 import time
 from typing import Optional
 
+from .. import utils
+from ..utils.time import save_data_to_memory
