71dad6d41d2df314f454f788ae1879b00fef3ec4
benluks <70067243+benluks@users.noreply.github.com>
2021-10-20 13:31:59 +0200

    refactor and finish audio file grab

--- src/speak/time.py
+++ src/speak/time.py
@@ -7,8 +7,6 @@ This module contains functions related to speaking commands
 
 import random
 import time
-import csv
-import os
 from src.utils.time import save_data_to_memory
 
 categories = ['hour', 'min', 'its_oclock', 'am_pm', 'teens', 'tens']
@@ -22,21 +20,45 @@ am = {True: 'am', False: 'pm'}
 data_map = save_data_to_memory()
 
 def tell_time() -> float:
+    """
+    grab current time and return audio files in the proper order
+    """
 
-    hour, min, am_pm = time.strftime("%H %M %p").split()
-    is_am = am_pm == 'AM'
+
+    hour, min, am_pm = time.strftime("%I %M %p").split()
     
-    audio_file_paths = grab_audio_from_time(int(hour), int(min), is_am)
-    concatenate_audio_files(*audio_file_paths)
-    return
+    hour_file = random.choice(data_map['hour'][spelled_hours[int(hour)]])
+    min_files = parse_mins(min)
+    am_pm_file = random.choice(data_map['am_pm'][am_pm.lower()])
 
-def concatenate_audio_files(*file_path):
+    # parse min. Different times require different treatment.
+    
+    concatenate_audio_files(hour_file, *min_files, am_pm_file)
+
+def concatenate_audio_files(*file_paths):
     """
     Concatenate audio files from their file paths
     """
-
-    
+    print([f for f in file_paths])
     return
+
+def parse_mins(min):
+    """
+    
+    """
+    if int(min) <= 10:
+         return (random.choice(data_map['min'][spelled_hours[int(min)]]))
+    elif int(min) > 10 and int(min) < 20:
+        return (random.choice(data_map['teens'][spelled_teens[int(min)-10]]))
+    else:
+        min_files = [random.choice(data_map['tens'][spelled_tens[int(min) // 10]])]
+        
+        # append the digit if the minute is not /on/ the ten (eg. is not 20, 30, etc...)
+        if int(min) % 10 != 0:
+            min_files += [random.choice(data_map['min'][spelled_hours[int(min) % 10]])]
+
+        return tuple(min_files)
+
 
 def grab_audio_from_time(hour: int, min: int, is_am: bool) -> None:
     
