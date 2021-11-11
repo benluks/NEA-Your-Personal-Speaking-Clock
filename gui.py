ef70c50e8cd7d0c14d61862ac22aaccf56000c1f
TechSword <sgf990223@gmail.com>
2021-11-11 13:18:55 +0100

    updated gui.py so it calls commands in commands.py and added change data path

--- gui.py
+++ gui.py
@@ -1,15 +1,21 @@
 import PySimpleGUI as sg
-from src.speak.time import tell_time
+from PySimpleGUI.PySimpleGUI import T
+# from src.speak.time import tell_time
 # from sd import voice_activation
+from src.commands import commands_map
+from src.speak.time import DATA_PATH, customize_data_path
 
 # Define the window's theme
 sg.theme('dark grey 9')
 
+cdp = 'Custom Data Path'
+
 # Define the window's contents
-layout = [[sg.Text("Time?")],
+layout = [[sg.Text("Would you like to hear the current time?")],
         #   [sg.Input(key='-INPUT-')],
         #   [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Button('Time'), sg.Button('Quit')]#,[sg.Button('Voice')]
+          [sg.Button('Time'), sg.Button('Quit'),sg.Button('Help')],
+          [sg.Button(cdp)]
           ]
 
 # Create the window
@@ -23,9 +29,21 @@ while True:
         break
     # Output a message to the window
     elif event == 'Time':
-        tell_time()
+        # tell_time()
+        command = commands_map['t']
+        command()
+        # sg.PopupTimed(action,title = 'The current time is')
         # sg.Popup(f'this is the current time')
         
+    elif event == cdp:
+        # sg.Popup(sg.Input(key='-INPUT-'))
+        cdp_folder = sg.popup_get_folder('Please type or browse to select a new data path folder')
+        sg.Popup('New Data Path', 'This is your new data path:', cdp_folder)
+        DATA_PATH = cdp_folder
+        
+        
+    elif event == 'Help':
+        sg.Popup(f"By default, the path to the audio data is \"data\/audio\/\" (relative to the root directory). If you'd like to change that, run this command. You'll be prompted to enter a new data path. As long as the entered path is visible from the root directory, it will work. Note that this command does not check whether the data is properly formatted.")
     # elif event == 'Voice':
         # voice_activation()
         # sg.Popup(f"the program is listening in the background")
